a95e6208a794f13f670599e301fb70a011bee712
Aneprados <apradoscamara@gmail.com>
2025-07-01 08:28:19 +0100

    Cambios en el main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from Lanzador import Lanzador
 
+def main():
+    # Aquí puedes agregar la lógica principal de tu programa
+    print("Ejecutando el programa...")
+    lanzador = Lanzador()
+    # Llama a métodos o realiza acciones con el objeto lanzador
+
 if __name__ == "__main__":
     main()
